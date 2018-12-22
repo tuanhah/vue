79aa2366bdacb10c8ef231698174d4d18e4c5e39
tuanhah <tuan.a.hoang.1998@gmail.com>
2018-12-22 17:32:43 +0700

    web

--- backend/app.js
+++ backend/app.js
@@ -5,42 +5,82 @@ var connection = require('./connection');
 var ROOT_URL = "/home/tuan/Desktop/Workspace/teneocto/lab01";
 var app = express();
 
-// var bodyParser = require("body-parser");
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+var bodyParser = require("body-parser");
+app.use(express.json({limit : '50mb'}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../frontend/public')));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.bodyParser());
+
+
 try {
     connection.con.connect();
     console.log("database success");
 }
 catch(err){
     throw err;
-    // console.log("have some error when connection with database");
+
 }
 
-
-
-app.get("/",function(req,res){
-    
+function select_all(res){
     connection.con.query('SELECT * from lab01', function (err, rows, fields) {
         if (err) throw err;
+        // console.log(rows);
+        res.send(rows);
+
+    })
+}
+app.get("/",function(req,res){
+    
+
+    // connection.con.query('SELECT * from lab01', function (err, rows, fields) {
+        // if (err) throw err;
       
-        console.log('The solution is: ', rows[0]);
-      })
+        // console.log('The solution is: ', rows[0]);
+      // })
     res.sendFile(path.join(ROOT_URL+"/frontend/public/page/upload.html"));
     
 });
+app.post("/get-data",function(req,res){
+    select_all(res);
+})
 app.post("/upload",function(req,res){
-    // var post_data = req.body;
+    var post_data = req.body;
     
-    console.log(req);
-    res.send({"dt" : true});
+    
+    var insert_sql = "INSERT INTO lab01 (name,category,description,price, image) VALUES ('"+post_data.name +"','"+post_data.category+"','"+post_data.description+"','"+post_data.price+"','"+post_data.image+"');";
+    // console.log(insert_sql);
+    connection.con.query(insert_sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+    select_all(res);
+    // res.send({"result":true});
+    
     
 });
 
-app.listen(3000, function () {
-    console.log('running at 3000 !');
+
+app.post("/update",function(req,res){
+    var post_data = req.body;
+    var update_sql="update lab01 set name ='"+post_data.name+"',price ='"+
+                    post_data.price+"',category ='"+ post_data.category
+                    +"',description ='" +post_data.description+"' where id =" +post_data.id+";";
+    connection.con.query(update_sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record update");
+    });
+    select_all(res);
+})
+
+app.post("/delete",function(req,res){
+    var post_data = req.body;
+    var delete_sql ="DELETE FROM lab01 where id ="+post_data.id+";";
+    connection.con.query(delete_sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record deleted");
+    });
+    select_all(res);
+})
+
+app.listen(6021, function () {
+    console.log('running at 6021 !');
 });
--- frontend/public/javascripts/upload.js
+++ frontend/public/javascripts/upload.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
+    
+
+    var old=[];
+    $.ajax({
+                    type :"post",
+                    url : "/get-data",
+                    // dataType : false,
+                    contentType : "application/json",
+                    // cache : false,
+                    success :function(data){
+                        // createTable(data);
+                        createTableWithoutDataTable(data);
+
+                        addOld(data);
+                    }
+    });
+        
+
+
     $("#submitForm").click(function(){
+                
         if (checkData()){
             var img="";
             base64( $('input[type="file"]'), function(data){
@@ -13,25 +33,24 @@ $(document).ready(function(){
                     "description" : $("#description").val(),
                     "image" : img,
                 }
-            // let req_data= new FormData();
-            // req_data.append("ca")
             
-
                 $.ajax({
                     type :"post",
                     url : "/upload",
-                    dataType : false,
+                    // dataType : false,
                     contentType : "application/json",
-                    cache : false,
-                    data : req_data,
-                    processData:false,
+                    // cache : false,
+                    data : JSON.stringify(req_data),
+                    // processData:false,
                     beforeSend: function(){
                         alert("Upload");
                     },
-                    // success : function(data){
-                    //     alert("upload success");
-                    //     // $.parseJSON(data);
-                    // }
+                    success : function(data){
+                        // createTable(data);
+                        createTableWithoutDataTable(data);
+                        old=[];
+                        addOld(data);     
+                    }
 
                 })
             
@@ -40,9 +59,167 @@ $(document).ready(function(){
         };
     });
 
-    function checkData(){
-        return true;
+    $(document).on('click','.btn-delete',function(){
+        var id_text = $(this).prop("id").split("-");
+        let data = {
+            id : id_text[2],
+        }
+        $.ajax({
+                    type :"post",
+                    url : "/delete",
+                    // dataType : false,
+                    contentType : "application/json",
+                    data : JSON.stringify(data),
+                    // cache : false,
+                    success :function(data){
+                        // createTable(data);
+                        createTableWithoutDataTable(data);
+                        old=[];
+                        addOld(data);
+                    }
+        });
+        
+
+    });
+
+
+    $(document).on('click','.btn-cancel',function(){
+        var id_text = $(this).prop("id").split("-");
+        var id = id_text[2];
+        var edit_button =`<button class="btn btn-info btn-sm btn-edit btn-table" id = "edit-button-`+id+`">Edit</button>`;
+        var delete_button = `<button class="btn btn-danger btn-sm btn-delete btn-table" id = "delete-button-`+id+`">Delete</button>`        
+        var row_id = "#td-btn-"+id;
+        $(this).remove();
+        var id_save_button = "#save-button-"+id;
+        $(id_save_button).remove();
+        $(row_id).append(edit_button);
+        $(row_id).append(delete_button);
+        var name = "#textarea-table-name-"+id;
+        var category = "#textarea-table-category-"+id;
+        var price = "#textarea-table-price-"+id;
+        var description = "#textarea-table-description-"+id;
+        for (var old_value of old){
+            if (old_value.id == id) {
+                $(name).val(old_value.name).attr('readonly', 'true');
+                $(category).val(old_value.category).attr('readonly', 'true');
+                $(price).val(old_value.price).attr('readonly', 'true');
+                $(description).val(old_value.description).attr('readonly', 'true');
+
+            }
+        }
+ 
+    })
+
+    $(document).on("click",'.btn-save',function(){
+        var id_text = $(this).prop("id").split("-");
+        var id = id_text[2];
+        var edit_button =`<button class="btn btn-info btn-sm btn-edit btn-table" id = "edit-button-`+id+`">Edit</button>`;
+        var delete_button = `<button class="btn btn-danger btn-sm btn-delete btn-table" id = "delete-button-`+id+`">Delete</button>`        
+        var row_id = "#td-btn-"+id;
+        $(this).remove();
+        var id_cancel_button = "#cancel-button-"+id;
+        $(id_cancel_button).remove();
+        $(row_id).append(edit_button);
+        $(row_id).append(delete_button);
+        var name = "#textarea-table-name-"+id;
+        var category = "#textarea-table-category-"+id;
+        var price = "#textarea-table-price-"+id;
+        var description = "#textarea-table-description-"+id;
+        // console.log($(category).html());
+        let data ={
+            "category" : $(category).val(),
+            "name"  : $(name).val(),
+            "price" : $(price).val(),
+            "description" : $(description).val(),
+            "id"    : id,
+        }
+        // console.log(data);
+
+        $.ajax({
+            url: '/update',
+            type: 'post',
+            contentType : "application/json",
+            data : JSON.stringify(data),
+                    // cache : false,
+            success :function(data){
+                        // createTable(data);
+                createTableWithoutDataTable(data);
+                old=[];
+                addOld(data);
+            }
+        })
+        
+    });
+    $(document).on("click",'.btn-edit',function() {
+        
+        var id_text = $(this).prop("id").split("-");
+        
+        var id = id_text[2];
+        var id_delete_button = "#delete-button-"+id;
+        
+        $(id_delete_button).remove();
+        $(this).remove();
+        var name = "#textarea-table-name-"+id;
+        var category = "#textarea-table-category-"+id;
+        var price = "#textarea-table-price-"+id;
+        var description = "#textarea-table-description-"+id;
+        $(name).removeAttr('readonly');
+        $(category).removeAttr('readonly');
+        $(price).removeAttr('readonly');
+        $(description).removeAttr('readonly');
+
+        
+        
+        var save_button = `<button class="btn btn-success btn-sm btn-save btn-table" id = "save-button-`+id+`">Save</button>`;
+        var cancel_button = `<button class="btn btn-secondary btn-sm btn-cancel btn-table" id = "cancel-button-`+id+`">Cancel</button>`;
+        var row_id = "#td-btn-"+id;
+        $(row_id).append(save_button);
+        $(row_id).append(cancel_button);
+        
+
+        
+
+    });
+
+
+    function addOld(data){
+        for (var j of data)
+        {
+            let item = 
+            {
+                "id" :j.id,
+                "name" : j.name,
+                "category" : j.category,
+                "price"     : j.price,
+                "image" : j.image,
+                "description": j.description, 
+            }
+            old.push(item);
+        }
+
     }
+
+    function checkData(){
+        // var lbl_error = document.getElementById("checkform");
+        
+        if (!$("#image").val()) {
+            $("#checkform").text("Image must be choosen!");
+            return false;
+        }
+        if (!$("#name").val()) {
+            $("#checkform").text("Name must be filled!");
+            return false;
+        }
+        if (!$("#price").val()) {
+            $("#checkform").text("Price must be filled!");
+            return false;
+        }
+        if (!$("#description").val()) {
+            $("#checkform").text("Description must be filled!");
+            return false;
+        }
+        return true;
+    };
     function base64(file, callback){
         var coolFile = {};
         function readerOnload(e){
@@ -59,7 +236,54 @@ $(document).ready(function(){
         coolFile.size = file.size;
         coolFile.filename = file.name;
         reader.readAsBinaryString(file);
-      }
+    };
+    function createTableWithoutDataTable(data){
+        $("#table-body").html('');
+        $.each(data,function(index, item) {
+            var id = parseInt(index,10)+1;
+            var new_row = `<tr id = `+item.id+`><td>`+id +`</td>`+`<td><textarea readonly id = "textarea-table-name-`+item.id+`">`+item.name+`</textarea></td><td><textarea readonly id = "textarea-table-category-`+item.id+`">`+item.category+`</textarea></td><td><textarea readonly id = "textarea-table-price-`+item.id+`">`+
+            item.price+`</textarea></td><td><textarea readonly id = "textarea-table-description-`+item.id+`">`+item.description+
+            `</textarea></td><td><img class="image-table" id = "image-`+item.id+`"src="data:image/png;base64,`+item.image+`" `+`style="height:100px;width:180px;"/></td>`
+            +`<td id ="td-btn-`+item.id+`"><button class="btn btn-info btn-sm btn-edit btn-table" id = "edit-button-`+item.id+`">Edit</button>`+
+            `<button class="btn btn-danger btn-sm btn-delete btn-table" id = "delete-button-`+item.id+`">Delete</button>`+`</td></tr>`;
+            $("#table").append(new_row);
+        });
+    }
+    function createTable(data){
+        var count=-1;
+        $("#table-data").DataTable({
+            data : data,
+            destroy: true,
+            columns :[
+                    { data: 'name' ,  title : 'name',},
+                    { data: 'category',title : 'category', },
+                    { data: 'price',title : 'price', },
+                    { data: 'description',title : 'description', },
+                    { 
+                        data: 'image',
+                        title : 'image',
+                       'render': function (src, type, full, meta) {
+                            if (count < data.length -1 ) count++;
+                        // console.log(data.length);
+                        // console.log(count);
 
+                            return '<img src="data:image/png;base64,'+data[count].image+ `"`+`style="height:100px;width:180px;"/>'`;
+                        }
+                    },
+                    {
+                        title : '',
+                        'render':function(data,type,full){
+                            return '<span style="padding-right:1rem;"><button class="btn btn-info btn-sm" id = "edit-button">Edit</button></span>'+'<button class="btn btn-danger btn-sm" id = "delete-button">Delete</button>';
+                        }
+                    }, 
+                
+            ],
+
+        })
+
+
+
+
+    };
     
 });
